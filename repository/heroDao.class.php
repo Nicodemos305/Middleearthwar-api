6586d93ae4ab417ee87962526c3f3b80e8f63afd
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-02-17 13:25:58 -0300

    Refactory entity hero for Http methods rest

--- repository/heroDao.class.php
+++ repository/heroDao.class.php
@@ -1,6 +1,6 @@
  <?php
 
-  include_once("../../entity/Hero.class.php");
+  include_once("../entity/Hero.class.php");
   include_once("DataSource.class.php");
 
 class HeroDao {
---
+++ services/hero.php
@@ -0,0 +1,33 @@
+<?php 
+  header('Content-type: application/json');
+  header('Access-Control-Allow-Origin: *');  
+  include_once('../repository/heroDao.class.php');
+
+  $msg = "";
+  $result = "";
+  $heroes = "";
+  $hero = "";
+  $heroDao = new HeroDao();
+
+  if($_SERVER['REQUEST_METHOD'] == "GET"){
+	 if(isset($_GET['id'])){
+	 	$hero = $heroDao->findOne($_GET['id']);
+	 	$result = array ('hero'=>$hero);
+	  }else{
+	  	 $heroes = $heroDao->findAll();
+	  	 $result = array ('heroes'=>$heroes);
+	  }
+  }else if($_SERVER['REQUEST_METHOD'] == "POST"){
+  	$name= $_POST['name'];
+ 	$hero = new Hero();
+    $hero->setName($name);
+    $hero->newHero();
+    $id = 1;
+    $heroDao->insert($hero,$id);
+  }else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+	 $id = $_GET['id'];
+     $msg =  $heroDao->delete($id);
+     $result = array ('msg'=>$msg);
+  }
+ 
+ echo json_encode($result);
