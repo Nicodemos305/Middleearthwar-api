35fa41617d5925172cc2dc4644256260eb0769ca
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-03-04 21:01:48 -0300

    Implements RankingDao

---
+++ repository/rankingDao.class.php
@@ -0,0 +1,18 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT']."/Rpgcloud/repository/DataSource.class.php");
+
+class RankingDao extends DataSource{
+
+	function rankingWinners(){
+		$sql = "select h.name,count(b.win_battle) as won from battle b inner join hero h on b.win_battle = h.id group by b.win_battle";
+		return parent::findAllEntity($sql);
+	}
+
+
+
+}
+
+
+
+
+
--- services/ranking/findAll.php
+++ services/ranking/findAll.php
@@ -1,8 +1,13 @@
 <?php
  header('Content-type: application/json');
  header('Access-Control-Allow-Origin: *');  
+ include_once($_SERVER['DOCUMENT_ROOT']."/Rpgcloud/repository/rankingDao.class.php");
 
+$rankingDao = new RankingDao();
 
+$result = $rankingDao->rankingWinners();
+
+ echo json_encode($result);
 
 
 
