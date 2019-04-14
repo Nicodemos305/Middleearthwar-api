d9aebbe6939f254d90ab1f1372b9619a411c22ae
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-04-14 15:24:13 -0300

    Implement crud of race

---
+++ repository/raceDao.class.php
@@ -0,0 +1,27 @@
+ <?php
+
+  include_once($_SERVER['DOCUMENT_ROOT']."/Rpgcloud/repository/DataSource.class.php");
+  include_once($_SERVER['DOCUMENT_ROOT']."/Rpgcloud/entity/Race.class.php");
+
+class RaceDao extends DataSource {
+
+	function insert(Race $race){
+	     $sql = "insert into race values(null,'".$race->getName()."','".$race->getHp()."','".$race->getMp()."','".$race->getAtk()."','".$race->getDefense()."','".$race->getAgility()."','".$race->getInteligence()."',0)";
+	     parent::insertEntity($sql);
+	 }
+
+	function findAll(){
+	     $sql = "select * from race";
+	     return parent::findAllEntity($sql);
+	}
+
+	function findOne($id){
+	     $sql = "select * from race where id =".$id;
+	     return parent::findOneEntity($sql);
+	}
+
+	function delete($id){
+		 $sql = "delete from race where id =".$id;
+		return parent::deleteEntity($sql);
+	}
+}
---
+++ services/race.php
@@ -0,0 +1,43 @@
+<?php 
+  header('Content-type: application/json');
+  header('Access-Control-Allow-Origin: *');  
+  include_once($_SERVER['DOCUMENT_ROOT']."/Rpgcloud/repository/raceDao.class.php");
+  include_once($_SERVER['DOCUMENT_ROOT']."/Rpgcloud/entity/Race.class.php");
+  $msg = "";
+  $result = "";
+  $race = "";
+  $raceDao = new RaceDao();
+
+  if($_SERVER['REQUEST_METHOD'] == "GET"){
+	 if(isset($_GET['id'])){
+	 	$race = $raceDao->findOne($_GET['id']);
+	 	$result = array ('race'=>$race);
+	  }else{
+	  	 $race = $raceDao->findAll();
+	  	 $result = array ('race'=>$race);
+	  }
+  }else if($_SERVER['REQUEST_METHOD'] == "POST"){
+  	$name= $_POST['name'];
+    $hp = $_POST['hp'];
+    $mp = $_POST['mp'];
+    $atk = $_POST['atk'];
+    $defense = $_POST['defense'];
+    $agility = $_POST['agility'];
+    $inteligence = $_POST['inteligence'];
+
+ 	  $raceInstance = new Race();
+    $raceInstance->setName($name);
+    $raceInstance->setHp($hp);
+    $raceInstance->setMp($mp);
+    $raceInstance->setAtk($atk);
+    $raceInstance->setDefense($defense);
+    $raceInstance->setAgility($agility);
+    $raceInstance->setInteligence($inteligence);
+    $raceDao->insert($raceInstance);
+  }else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+	 $id = $_GET['id'];
+     $msg =  $raceDao->delete($id);
+     $result = array ('msg'=>$msg);
+  }
+ 
+ echo json_encode($result);
