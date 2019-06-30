44695021063342a847157e603f31151606459d7c
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-06-30 20:46:18 -0300

    Implements Docker-compose

--- repository/battleDao.class.php
+++ repository/battleDao.class.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("../entity/Battle.class.php");
+include_once("../../entity/Battle.class.php");
 include_once("DataSource.class.php");
 
 class BattleDao extends DataSource{
--- repository/phaseDao.class.php
+++ repository/phaseDao.class.php
@@ -1,6 +1,6 @@
 <?php
   include_once("DataSource.class.php");
-  include_once("../entity/Phase.class.php");
+  include_once("../../entity/Phase.class.php");
 
 class PhaseDao extends DataSource {
 
