067bc393c045df4fbd940740db2065a448ca661c
Felipe Oliveira <felipe.oliveira@mac221023.local>
2019-07-18 13:21:30 -0300

    Implements switch in services for enhance the decision method execute #5

--- app/services/hero.php
+++ app/services/hero.php
@@ -14,32 +14,31 @@
   $heroDao = new HeroDao();
   $raceDao = new RaceDao();
 
-    if(isset($_SERVER['REQUEST_METHOD'])){
-      if($_SERVER['REQUEST_METHOD'] == "GET"){
-       if(isset($_GET['id'])){
-        $hero = $heroDao->findOne($_GET['id']);
-        $result = array ('hero'=>$hero);
-        }else{
-           $heroes = $heroDao->findAll();
-           $result = array ('heroes'=>$heroes);
-        }
-      }else if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $name= $post->name;
-        $race= $post->race;
-        $hero = new Hero();
-        $hero->setName($name);
-        $raceInstance = $raceDao->findByName($race);
-        $hero->newHero($raceInstance);
-        $id = 1;
-        $heroDao->insert($hero,$id);
-      }else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
-       $id = $_GET['id'];
-         $msg =  $heroDao->delete($id);
-         $result = array ('msg'=>$msg);
-      }
-    }
+    switch ($_SERVER['REQUEST_METHOD']) {
+      case "GET":
+        if(isset($_GET['id'])){
+          $hero = $heroDao->findOne($_GET['id']);
+          $result = array ('hero'=>$hero);
+          }else{
+            $heroes = $heroDao->findAll();
+            $result = array ('heroes'=>$heroes);
+          }
+          break;
+      case "POST":
+          $name= $post->name;
+          $race= $post->race;
+          $hero = new Hero();
+          $hero->setName($name);
+          $raceInstance = $raceDao->findByName($race);
+          $hero->newHero($raceInstance);
+          $id = 1;
+          $heroDao->insert($hero,$id);
+          break;
+      case "DELETE":
+          $id = $_GET['id'];
+          $msg =  $heroDao->delete($id);
+          $result = array ('msg'=>$msg);
+          break;
+  }
 
- 
  echo json_encode($result);
-
-
--- app/services/race.php
+++ app/services/race.php
@@ -2,7 +2,6 @@
   header('Content-type: application/json');
   header('Access-Control-Allow-Origin: *');
 
-
   include_once("../repository/raceDao.class.php");
   include_once("../entity/Race.class.php");
   
@@ -11,38 +10,41 @@
   $msg = "";
   $result = "";
   $race = "";
-  $raceDao = new RaceDao();
+  $raceDao = new RaceDao(); 
+  switch ($_SERVER['REQUEST_METHOD']) {
+    case "GET":
+        if(isset($_GET['id'])){
+          $race = $raceDao->findOne($_GET['id']);
+          $result = array ('race'=>$race);
+        }else{
+            $race = $raceDao->findAll();
+            $result = array ('race'=>$race);
+        }
+        break;
+    case "POST":
+        $name= $post->name;
+        $hp = $post->hp;
+        $mp = $post->mp;
+        $atk = $post->atk;
+        $defense = $post->defense;
+        $agility = $post->agility;
+        $inteligence = $post->inteligence;
 
-  if($_SERVER['REQUEST_METHOD'] == "GET"){
-	 if(isset($_GET['id'])){
-	 	$race = $raceDao->findOne($_GET['id']);
-	 	$result = array ('race'=>$race);
-	  }else{
-	  	 $race = $raceDao->findAll();
-	  	 $result = array ('race'=>$race);
-	  }
-  }else if($_SERVER['REQUEST_METHOD'] == "POST"){
-  	$name= $post->name;
-    $hp = $post->hp;
-    $mp = $post->mp;
-    $atk = $post->atk;
-    $defense = $post->defense;
-    $agility = $post->agility;
-    $inteligence = $post->inteligence;
+        $raceInstance = new Race();
+        $raceInstance->setName($name);
+        $raceInstance->setHp($hp);
+        $raceInstance->setMp($mp);
+        $raceInstance->setAtk($atk);
+        $raceInstance->setDefense($defense);
+        $raceInstance->setAgility($agility);
+        $raceInstance->setInteligence($inteligence);
+        $raceDao->insert($raceInstance);
+        break;
+    case "DELETE":
+        $id = $_GET['id'];
+        $msg =  $raceDao->delete($id);
+        $result = array ('msg'=>$msg);
+        break;
+}
 
- 	  $raceInstance = new Race();
-    $raceInstance->setName($name);
-    $raceInstance->setHp($hp);
-    $raceInstance->setMp($mp);
-    $raceInstance->setAtk($atk);
-    $raceInstance->setDefense($defense);
-    $raceInstance->setAgility($agility);
-    $raceInstance->setInteligence($inteligence);
-    $raceDao->insert($raceInstance);
-  }else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
-	 $id = $_GET['id'];
-     $msg =  $raceDao->delete($id);
-     $result = array ('msg'=>$msg);
-  }
- 
- echo json_encode($result);
+echo json_encode($result);
