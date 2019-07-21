5cd782a8047bf21c5852af46188788a472a9c74e
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-07-20 21:26:54 -0300

    Clean code battlecore

--- app/services/battle/battleCore.class.php
+++ app/services/battle/battleCore.class.php
@@ -22,52 +22,52 @@ class BattleCore{
 			$hp1 = $battleAux['hp_hero_one'];
 			$d6 = rand(1,6);
 			$random = rand(1,$enemy['atk'])+$d6;
-
-			//esquiva
-			$esquiva = rand(0,$playerOne['agility']);
-			if($esquiva == $playerOne['agility']){
-				$phase->setDescription("O Herói ".$playerOne['name']." esquivou.");
-			}else{
-				$phase = $this->damage($battleAux, $phase, $random, $playerOne, $battleDao);
-			}
-		
-
-			$phase->setBattle_id($battleAux['id']);
-			$phase->setPlayer_id($battleAux['id_hero_two']);
-			$phaseDao->insert($phase);
-
+			$phase = $this->damage($battleAux, $phase, $random, $playerOne, $battleDao);
+			$id = $battleAux['id_hero_two'];
+			$this->passPhase($phase, $battleAux, $id, $phaseDao);
 		}else if($recentPhase['id_hero_one'] == $battleAux['id_hero_two']){
-				$hp1 = $battleAux['hp_hero_two'];
-				$d6 = rand(1,6);
-				$random = rand(1,$playerOne['atk']) + $d6;
-	
-			//esquiva
-				$esquiva = rand(0,$enemy['agility']);
-				if($esquiva == $enemy['agility']){
-					$phase->setDescription("O Herói ".$enemy['name']." esquivou.");
-				}else{
-					$phase = $this->damage($battleAux, $phase, $random, $enemy, $battleDao);
-				}
-			//esquiva
-			$phase->setBattle_id($battleAux['id']);
-			$phase->setPlayer_id($playerOne['id']);
-			$phaseDao->insert($phase);
+			$hp1 = $battleAux['hp_hero_two'];
+			$d6 = rand(1,6);
+			$random = rand(1,$playerOne['atk']) + $d6;
+			$phase = $this->damage($battleAux, $phase, $random, $enemy, $battleDao);
+			$this->passPhase($phase, $battleAux, $playerOne['id'], $phaseDao);
 		}
 	}
 
-	function damage($battle, $phase, $random, $hero, $battleDao){
-		$damage = $this->critical($random);
-		if($hero['id'] == $battle['id_hero_one']){
-			$hp1 = $battle['hp_hero_one'] - $damage;
-			$battleDao->hpMinusPlayerOne($hp1,$battle['id']);
-		}
+	function passPhase($phase, $battle, $player, $phaseDao){
+		$phase->setBattle_id($battle['id']);
+		$phase->setPlayer_id($player);
+		$phaseDao->insert($phase);
 
-		if($hero['id'] == $battle['id_hero_two']){
-			$hp1 = $battle['hp_hero_two'] - $damage;
-			$battleDao->hpMinusPlayerTwo($hp1,$battle['id']);
+	}
+
+	function damage($battle, $phase, $random, $hero, $battleDao){
+		$esquivou = $this->esquiva($hero);
+
+		if($esquivou){
+			$phase->setDescription("O Herói ".$hero['name']." esquivou.");
+		}else{
+			$damage = $this->critical($random);
+			if($hero['id'] == $battle['id_hero_one']){
+				$hp1 = $battle['hp_hero_one'] - $damage;
+				$battleDao->hpMinusPlayerOne($hp1,$battle['id']);
+			}
+
+			if($hero['id'] == $battle['id_hero_two']){
+				$hp1 = $battle['hp_hero_two'] - $damage;
+				$battleDao->hpMinusPlayerTwo($hp1,$battle['id']);
+			}
+			$phase->setDescription("O Herói ".$hero['name']." recebeu ".$damage." de dano crítico.");
 		}
-		$phase->setDescription("O Herói ".$hero['name']." recebeu ".$damage." de dano crítico.");
 		return $phase;
+	}
+
+	function esquiva($hero){
+		$esquiva = rand(0,$hero['agility']);
+		if($esquiva == $hero['agility']){
+			return true;
+		}
+		return false;
 	}
 
 	function critical($damage){
