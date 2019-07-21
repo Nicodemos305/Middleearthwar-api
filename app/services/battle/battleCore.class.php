62bcab15c9b0ed7bb69b408dc7d2b0f0441e721e
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-07-21 17:09:15 -0300

    Refactory clean code

--- app/services/battle/battleCore.class.php
+++ app/services/battle/battleCore.class.php
@@ -7,41 +7,46 @@ class BattleCore{
 
 	function routePhaseWithCpu($battleAux,$recentPhase,$phase,$enemy,$playerOne,$phaseDao,$battleDao){
 		$hp1 = 0;
+	
 		if (is_array($battleAux)){
 			$recentPhase = $phaseDao->findPhase($battleAux['id']);
 		}else{
 			$recentPhase = $phaseDao->findPhase($battleAux);	
 		}
 
+		$isHeroOne = $recentPhase['id_hero_one'] == $battleAux['id_hero_one'];
+		$isHeroTwo = $recentPhase['id_hero_one'] == $battleAux['id_hero_two'];
+
 		if($recentPhase == null && is_array($battleAux)){
 			$phase->setBattle_id($battleAux['id']);
 			$phase->setPlayer_id($battleAux['id_hero_one']);
 			$phase->setDescription("Iniciou o combate entre o heroi ".$playerOne['name']." e o Heroi ".$enemy['name']);
 			$phaseDao->insert($phase);
-		}else if($recentPhase['id_hero_one'] == $battleAux['id_hero_one']){
-			$hp1 = $battleAux['hp_hero_one'];
-			$d6 = rand(1,6);
-			$random = rand(1,$enemy['atk'])+$d6;
+		}else if($isHeroOne){
+			$random = $this->rollD6($enemy['atk']);
 			$phase = $this->damage($battleAux, $phase, $random, $playerOne, $battleDao);
 			$this->passPhase($phase, $battleAux, $battleAux['id_hero_two'], $phaseDao);
-		}else if($recentPhase['id_hero_one'] == $battleAux['id_hero_two']){
-			$hp1 = $battleAux['hp_hero_two'];
-			$d6 = rand(1,6);
-			$random = rand(1,$playerOne['atk']) + $d6;
+		}else if($isHeroTwo){
+			$random = $this->rollD6($playerOne['atk']);
 			$phase = $this->damage($battleAux, $phase, $random, $enemy, $battleDao);
 			$this->passPhase($phase, $battleAux, $playerOne['id'], $phaseDao);
 		}
+	}
+
+	function rollD6($atk){
+		$d6 = rand(1,6);
+		$random = rand(1,$atk) + $d6;
+		return $random;
 	}
 
 	function passPhase($phase, $battle, $player, $phaseDao){
 		$phase->setBattle_id($battle['id']);
 		$phase->setPlayer_id($player);
 		$phaseDao->insert($phase);
-
 	}
 
 	function damage($battle, $phase, $random, $hero, $battleDao){
-		$esquivou = $this->esquiva($hero);
+		$esquivou = rand(0,$hero['agility']) == $hero['agility'];
 
 		if($esquivou){
 			$phase->setDescription("O Herói ".$hero['name']." esquivou.");
@@ -59,14 +64,6 @@ class BattleCore{
 			$phase->setDescription("O Herói ".$hero['name']." recebeu ".$damage." de dano crítico.");
 		}
 		return $phase;
-	}
-
-	function esquiva($hero){
-		$esquiva = rand(0,$hero['agility']);
-		if($esquiva == $hero['agility']){
-			return true;
-		}
-		return false;
 	}
 
 	function critical($damage){
