6393cbb552fcd51dd7364c1d9ee2758b0fb7f087
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-07-26 00:54:16 -0300

    Implements Super class Race in Hero entity

--- app/entity/hero.class.php
+++ app/entity/hero.class.php
@@ -1,15 +1,8 @@
 <?php
+include_once "/var/www/html/entity/Race.class.php";
+class Hero extends Race{
 
-class Hero {
-	private $uuid; 
-	private $name;
 	private $race;
-	private $healthPoint;
-	private $magicPoint;
-	private $atk;
-	private $defense;
-	private $agility;
-	private $inteligence;
 
 	public function __construct($name, $race, $healthPoint, $magicPoint, $atk, $defense, $agility, $inteligence) {
         $this->name = $name;
@@ -22,78 +15,6 @@ class Hero {
         $this->inteligence = $inteligence;
     }
   
-	public function getUuid(){
-		return $this->uuid;
-	}
-
-	public function setUuid($uuid){
-		$this->uuid = $uuid;
-	}
-
-	public function getName(){
-		return $this->name;
-	}
-
-	public function setName($name){
-		$this->name = $name;
-	}
-
-	public function getRace(){
-		return $this->race;
-	}
-
-	public function setRace($race){
-		$this->race = $race;
-	}
-
-	public function getHealthpoint(){
-		return $this->healthPoint;
-	}
-
-	public function setHealthpoint($healthPoint){
-		$this->healthPoint = $healthPoint;
-	}
-
-	public function getMagicPoint(){
-		return $this->magicPoint;
-	}
-
-	public function setMagicPoint($magicPoint){
-		$this->magicPoint = $magicPoint;
-	}
-
-	public function getAtk(){
-		return $this->atk;
-	}
-
-	public function setAtk($atk){
-		$this->atk = $atk;
-	}
-
-	public function getDefense(){
-		return $this->defense;
-	}
-
-	public function setDefense($defense){
-		$this->defense = $defense;
-	}
-
-	public function getAgility(){
-		return $this->agility;
-	}
-
-	public function setAgility($agility){
-		$this->agility = $agility;
-	}
-
-	public function getInteligence(){
-		return $this->inteligence;
-	}
-
-	public function setInteligence($inteligence){
-		$this->inteligence = $inteligence;
-	}
-
 	public function newHero($race){
 	 		$this->setRace($race['name']);
 	  		$this->setHealthpoint($race['hp']);
