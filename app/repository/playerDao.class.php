65f42e2c48a22844c6a80d8f944230596b43b0c5
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-07-26 02:02:27 -0300

    fix Direct use of REQUEST_METHOD Superglobal detected.

--- app/repository/playerDao.class.php
+++ app/repository/playerDao.class.php
@@ -1,6 +1,7 @@
 <?php
+
 include_once "/var/www/html/entity/Player.class.php";
-include_once "DataSource.class.php";
+include_once "/var/www/html/repository/DataSource.class.php";
 
 class PlayerDao extends DataSource {
 
