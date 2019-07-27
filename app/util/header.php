5e6d51b0a29a0986d8aa2e581c61cd0e5eb70b26
Felipe Nicodemos de Oliveira <felipe.nicodemos@hotmail.com>
2019-07-27 00:24:39 -0300

    header refactory

--- app/util/header.php
+++ app/util/header.php
@@ -1,6 +1,6 @@
 <?php
- header 'Content-type: application/json';
- header 'Access-Control-Allow-Origin: *';
+ header('Content-type: application/json');
+ header('Access-Control-Allow-Origin: *');
  $request = filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_SANITIZE_STRING);
  $post = json_decode(file_get_contents('php://input'));
  $result = null;
